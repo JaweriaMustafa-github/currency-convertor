8522ccda357946f0ab1bb903caa63cf58a4fcd8a
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-22 23:32:14 +0500

    edited index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const currency = {
-    USD: 1,
+    USD: 1, //base currency
     EUR: 0.91,
     GBP: 0.76,
     INR: 74.57,
@@ -25,8 +26,8 @@ let userAnswer = await inquirer.prompt([
         type: 'number'
     }
 ]);
-let fromAmount = currency[userAnswer.from];
-let toAmount = currency[userAnswer.to];
+let fromAmount = currency[userAnswer.from]; //exchange rate;
+let toAmount = currency[userAnswer.to]; //exchange rate;
 let amount = userAnswer.amount;
 let baseAmount = amount / fromAmount;
 let convertedAmount = baseAmount * toAmount;
